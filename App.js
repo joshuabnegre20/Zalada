e73f1f0fb4930aeba6406f98414a6e1901c82c2f
Busalanan_FY/github_username <fayemarie@gmail.com>
2025-08-15 08:10:49 +0800

    Color changer App

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+//import CounterApp from './CounterApp';
+import ColorChangerApp from './ColorChangerApp';
+function App() {
+  return <ColorChangerApp />
+   
+  
+}
+export default App;
